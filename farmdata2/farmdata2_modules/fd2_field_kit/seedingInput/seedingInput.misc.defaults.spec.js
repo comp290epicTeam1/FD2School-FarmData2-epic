b76be6eb2d1cc1db14807d9e45ad203c1c3b2e15
jamesng5 <nguyenja@dickinson.edu>
2023-04-17 22:56:45 -0400

    Test the comment box

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
@@ -1,3 +1,10 @@
 describe('Test Seeding Input Miscellaneous Defaults', () =>{
+    beforeEach(()=>{
+        cy.login("manager1", "farmdata2")
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    })
 
+    it('', ()=>{
+        cy.get("[data-cy=comments").should("be.empty")
+    })
 })
