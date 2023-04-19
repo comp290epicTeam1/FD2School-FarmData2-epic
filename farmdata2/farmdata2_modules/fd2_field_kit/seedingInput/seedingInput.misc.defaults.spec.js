be31ffee0208b191f83b5ebbe2b5e4792bcbddd2
tainguyen103 <tainguyen103200@gmail.com>
2023-04-19 13:31:59 -0400

    tested to check that submit button is disabled

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
@@ -1,3 +1,20 @@
 describe('Test Seeding Input Miscellaneous Defaults', () =>{
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-field-kit/seedingInput/seedingInput.html")
+    })
 
+    it("Check that the SUBMIT button is disabled", () => {
+        cy.get("[data-cy=submit-button]").should("be.disabled")
+    })
 })
+
+
+
+
+
+
+
+
+
+
