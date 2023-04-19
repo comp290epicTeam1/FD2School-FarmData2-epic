6efac33341b4eb0e79bd64845926e0f520e9c612
jamesng5 <nguyenja@dickinson.edu>
2023-04-19 13:51:00 -0400

    Test for Submit button

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.misc.defaults.spec.js
@@ -4,7 +4,12 @@ describe('Test Seeding Input Miscellaneous Defaults', () =>{
         cy.visit('/farm/fd2-field-kit/seedingInput')
     })
 
-    it('', ()=>{
+    it('Test the comment text box is empty and enable', ()=>{
         cy.get("[data-cy=comments").should("be.empty")
+        cy.get("[data-cy=comments").should('not.be.disabled')
+    })
+
+    it('Test the Submit button box is labeled', ()=>{
+        cy.get("[data-cy=submit-button]").should("have.text","Submit").should('be.visible')
     })
 })
