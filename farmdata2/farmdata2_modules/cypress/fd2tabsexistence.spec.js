0e8d4e1804a4f904197699ea6d93882d2a80c01e
jamesng5 <nguyenja@dickinson.edu>
2023-04-28 01:55:49 -0400

    Test tabs existence for worker account

--- farmdata2/farmdata2_modules/cypress/fd2tabsexistence.spec.js
+++ farmdata2/farmdata2_modules/cypress/fd2tabsexistence.spec.js
@@ -25,4 +25,28 @@ describe("Test tabs existence for each user's account", ()=>{
         })
     })
 
+
+    /**
+     * Check the existence of FieldKit and BarnKit, but not FD2 Config 
+     * tabs if a worker account is logged in.
+     */
+    context('Worker account is logged in', () => {
+    
+        beforeEach(()=>{
+            cy.login("worker1", "farmdata2")
+            cy.visit('/farm')
+        })
+    
+        it("Check the FieldKit tab", () => {
+            cy.get(".tabs--primary").get("a").contains("FieldKit")
+        })
+    
+        it("Check the BarnKit tab", () => {
+            cy.get(".tabs--primary").get("a").contains("BarnKit")
+        })
+    
+        it("Check the FD2 Config tab", () => {
+            cy.get(".tabs--primary").get("a").get("FD2 Config").should("not.exist")
+        })
+    })
 })
