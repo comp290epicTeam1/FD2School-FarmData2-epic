8df47877c36fc1346c50a64e76320e6af8dcec01
jamesng5 <nguyenja@dickinson.edu>
2023-04-28 01:37:35 -0400

    Add test for the existence of FieldKit, BarnKit, FD2 Config tabs when manager is logged in

--- farmdata2/farmdata2_modules/cypress/fd2tabsmanager.spec.js
+++ farmdata2/farmdata2_modules/cypress/fd2tabsmanager.spec.js
@@ -1,8 +1,24 @@
+/**
+ * Check the existence of FieldKit, BarnKit, and FD2 Config tabs if
+ * a manager account is logged in.
+ */
+
 describe('Test Seeding Input Miscellaneous Defaults', () =>{
     beforeEach(()=>{
         cy.login("manager1", "farmdata2")
         cy.visit('/farm')
     })
 
+    it("Check the FieldKit tab", () => {
+        cy.get(".tabs--primary").get("a").contains("FieldKit")
+    })
+
+    it("Check the BarnKit tab", () => {
+        cy.get(".tabs--primary").get("a").contains("BarnKit")
+    })
+
+    it("Check the FD2 Config tab", () => {
+        cy.get(".tabs--primary").get("a").contains("FD2 Config")
+    })
 
 })
